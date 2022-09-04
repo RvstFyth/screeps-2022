36abf167f7bfff592ca14be956e2ae84df231847
RvStFyth <r.vansintfeyth@protonmail.com>
2022-09-04 14:46:02 +0200

    Delete memory for creeps that died

--- main.js
+++ main.js
@@ -13,4 +13,11 @@ module.exports.loop = () => {
     for (const room of Object.values(Game.rooms)) {
         if (room.controller.my) controllers['room'].run(room);
     }
+
+    // Delete memory from creeps that died
+    for (const name in Memory.creeps) {
+        if (!(name in Game.creeps)) {
+          delete Memory.creeps[name];
+        }
+    }
 }
