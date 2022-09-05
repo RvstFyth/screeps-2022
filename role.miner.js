aa6bed315f18e35a1122aca652964f604dd03b39
RvStFyth <r.vansintfeyth@protonmail.com>
2022-09-05 21:23:13 +0200

    removed debug lines

--- role.miner.js
+++ role.miner.js
@@ -7,11 +7,9 @@ module.exports = {
     run: function(creep)
     {
         if (creep.pos.x != creep.memory.targetX || creep.pos.y != creep.memory.targetY) {
-            creep.say(1)
             creep.moveTo(creep.memory.targetX, creep.memory.targetY, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
         else {
-            creep.say(2)
             const source = Game.getObjectById(creep.memory.targetID);
             creep.harvest(source);
         }
